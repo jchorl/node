a6ca83ea0e5ba7ea5651baf74b92f9222ff1326b
Josh Chorlton <jchorlton@gmail.com>
2020-04-18 11:23:12 -0700

    deploy to latest-arm

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,4 @@ echo "$2" | docker login -u "$1" --password-stdin
 
 docker images
 
-docker tag jchorl/node:latest-arm jchorl/node:latest
-docker push jchorl/node:latest
+docker push jchorl/node:latest-arm
